c4eadc05fbebf1384f9b3a6ce55f5d32bb2dc40d
Rodrigo Castrejón <47370853+rcastrejon@users.noreply.github.com>
2023-10-06 20:01:14 -0600

    add basic site layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: colors.stone["100"],
+        foreground: colors.stone["950"],
+        "muted-foreground": colors.stone["400"],
+      },
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        serif: ["Kaisei Decol", ...defaultTheme.fontFamily.serif],
+      },
+    },
+  },
+  plugins: [],
+};
